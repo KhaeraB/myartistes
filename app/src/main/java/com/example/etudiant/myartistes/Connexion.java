5ceaecf33876c8fcbff6069fc75028a65f6c8ed9
etudiant <troiswa>
2018-10-15 15:22:26 +0200

    LauchBoard

---
+++ app/src/main/java/com/example/etudiant/myartistes/Connexion.java
@@ -0,0 +1,16 @@
+package com.example.etudiant.myartistes;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+public class Connexion extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.connexion);
+
+    }
+
+}
